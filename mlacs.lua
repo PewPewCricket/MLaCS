ce13d46b04c72f62f0f15e03ca292c51e4b52e39
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2023-08-02 13:53:08 -0500

    Add files via upload

--- mlacs.lua
+++ mlacs.lua
@@ -1,13 +1,11 @@
 local term = require("term")
-local color = require("colors")
 local component = require("component")
-local text = require("text")
-local shell = require("shell")
+local data = require("data")
+local fs = require("filesystem")
 local launch = component.getPrimary("launch_pad")
 --define variables
 local X = nil
 local Z = nil
-local launchCodes = "0"
 --local yield = nil
 
 --text printing
@@ -112,82 +110,89 @@ if userInput == "1" then
     --end
     --goto start
     
-  elseif userInput == "2" then
+elseif userInput == "2" then
   --target select
-    term.clear()
-    print("Please select x and z position")
-    io.write("X: ")
-    local  sX = io.read()
-    X = tonumber(sX)
-    os.sleep(0.2)
-    term.setCursor(1, 3)
-    io.write("Z: ")
-    local sZ = io.read()
-    Z = tonumber(sZ)
+  term.clear()
+  print("Please select x and z position")
+  io.write("X: ")
+  local  sX = io.read()
+  X = tonumber(sX)
+  os.sleep(0.2)
+  term.setCursor(1, 3)
+  io.write("Z: ")
+  local sZ = io.read()
+  Z = tonumber(sZ)
     
-    if X == nil or Z == nil then
-      term.setCursor(1, 5)
-      io.stderr:write("invalid input")
-      os.sleep(1)
-      goto start
-    else
-      os.sleep(0.2)
-      term.setCursor(1, 5)
-      print("Target Selected")
-      os.sleep(1)
-      goto start
-    end
-  
-  elseif userInput == "3" then
-  --config
-    ::config::
-    term.clear()
-    print("Please Select an Option:")
-    term.setCursor(1, 3)
-    print("1: Change launch codes")
-    print("2: Back")
-    term.setCursor(1, 6)
-    userInputCfg = io.read()
-    
-    if userInputCfg == "1" then
-      term.clear()
-      io.write("Input current codes: ")
-      local userCodesInput = io.read()
-      if userCodesInput == launchCodes then
-        term.setCursor(1, 3)
-        io.write("Input new codes: ")
-        launchCodes = io.read()
-        os.sleep(0.5)
-        term.setCursor(1, 5)
-        print("Codes changed")
-        os.sleep(1)
-        goto start
-      else
-        term.setCursor(1, 5)
-        io.stderr:write("Invalid codes")
-        os.sleep(1)
-        goto config
-      end
-      
-      elseif userInputCfg == "2" then
-        os.sleep(1)
-        goto start
-    
-      else
-        term.setCursor(1, 8)
-        io.stderr:write("invalid input")
-        os.sleep(1)
-        goto config
-    end
-    
-  elseif userInput == "4" then
-  --exit
-    term.clear()
-    os.exit()
-    
-  else
+  if X == nil or Z == nil then
+    term.setCursor(1, 5)
     io.stderr:write("invalid input")
     os.sleep(1)
     goto start
+  else
+    os.sleep(0.2)
+    term.setCursor(1, 5)
+    print("Target Selected")
+    os.sleep(1)
+    goto start
+  end
+  
+elseif userInput == "3" then
+  --config
+  ::config::
+  term.clear()
+  print("Please Select an Option:")
+  term.setCursor(1, 3)
+  print("1: Change launch codes")
+  print("2: Back")
+  term.setCursor(1, 6)
+  userInputCfg = io.read()
+    
+  if userInputCfg == "1" then
+    term.clear()
+    local codesFile = io.open("/etc/Launchcodes.txt", "r")
+    oldCodes = codesFile:read("*l")
+    codesFile:close()
+    io.write("Input current codes: ")
+    local userCodesInput = io.read()
+    if userCodesInput == oldCodes then
+      term.setCursor(1, 3)
+      io.open("/etc/Launchcodes.txt", "w")
+      io.write("Input new codes: ")
+      local newCodes = io.read()
+      codesFile:write(newCodes)
+      codesFile:close()
+      os.sleep(0.5)
+      term.setCursor(1, 5)
+      print("Codes changed")
+      os.sleep(1)
+      goto config
+    else
+      term.setCursor(1, 5)
+      io.stderr:write("Invalid codes")
+      codesFile:close()
+      os.sleep(1)
+      goto config
+    end
+      
+  elseif userInputCfg == "2" then
+      os.sleep(1)
+      goto start
+    
+  else
+      term.setCursor(1, 8)
+      io.stderr:write("invalid input")
+      os.sleep(1)
+      goto config
+  end
+    
+elseif userInput == "4" then
+  --exit
+  term.clear()
+  os.exit()
+    
+else
+  io.stderr:write("invalid input")
+  os.sleep(1)
+  goto start
     
 end
